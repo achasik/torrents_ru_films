f52f379c17819457fa36fc4cd0e53c34eb2fc807
Andrey Chasovskoy <achasik@outlook.com>
2017-11-23 18:51:18 +0400

    rutor

--- update.sql
+++ update.sql
@@ -1,1 +1,4 @@
-update trackers set active=0 where id=2
+update trackers set active=1 where id=2;
+update feeds set feed='http://alt.rutor.info/rss.php?category=1' where id =6;
+update feeds set feed='http://alt.rutor.info/rss.php?category=5' where id =7;
+
